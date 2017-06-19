93915535aa5ddb9408257e032fa9bb7344e0ced6
murraybrad13 <murraybrad13@gmail.com>
2017-06-19 16:49:40 -0400

    Reformatted + deduped + comments bp script

---
+++ NTLA/PR-11410_Somatic_refined.sh
@@ -0,0 +1,82 @@
+## STEP 1: Mark Duplicates
+## *merged.bam is after merging across lanes, already done when downloading from SRA
+## Do this on one sample at a time
+<PATH>/java -Djava.io.tmpdir=<PATH> -Xmx24576m -XX:ParallelGCThreads=5 \
+	-jar <PATH>/picard-tools/picard-tools-1.83/MarkDuplicates.jar \
+	CREATE_INDEX=true \
+	MAX_RECORDS_IN_RAM=2000000 \
+	VALIDATION_STRINGENCY=SILENT \
+	M=$Sample_dir/qc/FXX.dedup.metrics \
+	I=$Sample_dir/analysis/FXX.merged.bam \
+	O=$Sample_dir/analysis/FXX.dedup.bam
+
+
+
+## STEP 2: Realignment Target Creator
+## Doing this on all samples at once
+<PATH>/java -Djava.io.tmpdir=<PATH> -Xmx73728m \
+	-jar <PATH>/GenomeAnalysisTK/GenomeAnalysisTK-3.4-0/GenomeAnalysisTK.jar \
+	-T RealignerTargetCreator \
+	--interval_padding 200 \
+	-rf BadCigar \
+	-nt 24 \
+	-R <PATH>/Mus_musculus/GRCm38.70/Mus_musculus.GRCm38.70.dna.fasta \
+	-I $Sample_dir/analysis/FXX.dedup.bam \
+	-I $Sample_dir/analysis/FXX.dedup.bam \
+	-I $Sample_dir/analysis/FXX.dedup.bam \
+	-o $Sample_dir/analysis/FXX.for_realigner.joint.intervals
+
+
+
+## STEP 3: Indel Realignment
+## Doing this on all samples at once
+## will need to generate this file -nWayOut $Sample_dir/analysis/for_realigner.joint.map
+<PATH>/java -Djava.io.tmpdir=<PATH> -Xmx73728m \
+ 	-jar <PATH>/GenomeAnalysisTK/GenomeAnalysisTK-3.4-0/GenomeAnalysisTK.jar \
+ 	-T IndelRealigner \
+ 	-rf BadCigar \
+ 	--consensusDeterminationModel USE_READS \
+ 	-dcov 1000 \
+ 	-R <PATH>/Mus_musculus/GRCm38.70/Mus_musculus.GRCm38.70.dna.fasta \
+ 	-I $Sample_dir/analysis/FXX.dedup.bam \
+ 	-I $Sample_dir/analysis/FXX.dedup.bam \
+ 	-I $Sample_dir/analysis/FXX.dedup.bam \
+ 	-targetIntervals $Sample_dir/analysis/FXX.for_realigner.joint.intervals \
+ 	-nWayOut $Sample_dir/analysis/for_realigner.joint.map
+
+
+
+## STEP 4: Generatinng BQSR Covariates
+## use known site vcfs
+<PATH>/java -Djava.io.tmpdir=<PATH> -Xmx24576m \
+	-jar <PATH>/GenomeAnalysisTK/GenomeAnalysisTK-3.4-0/GenomeAnalysisTK.jar \
+	-T BaseRecalibrator \
+	--interval_padding 200 \
+	--downsample_to_fraction 0.1 \
+	-rf BadCigar \
+	-nct 4 \
+	-R <PATH>/Mus_musculus/GRCm38.70/Mus_musculus.GRCm38.70.dna.fasta \
+	-I $Sample_dir/analysis/FXX.dedup.joint.realigned.bam \
+	-o $Sample_dir/qc/FXX.recal.grp \
+	-knownSites <PATH>/Mus_musculus/GRCm38.70/dbSNP_v138.vcf.gz \
+	-knownSites <PATH>/Mus_musculus/GRCm38.70/mgp.v3.indels.clean.vcf.gz
+
+
+
+## STEP 5: Applying BQSR Adjustments
+$java_bin_dir/bin/java -Djava.io.tmpdir=<PATH> -Xmx24576m \
+ 	-jar <PATH>/GenomeAnalysisTK/GenomeAnalysisTK-3.4-0/GenomeAnalysisTK.jar \
+ 	-T PrintReads \
+ 	-nct 4 \
+ 	-rf BadCigar \
+ 	--disable_indel_quals \
+ 	--emit_original_quals \
+ 	-R <PATH>/Resources/Mus_musculus/GRCm38.70/Mus_musculus.GRCm38.70.dna.fasta \
+ 	-I $Sample_dir/analysis/FXX.dedup.joint.realigned.bam \
+ 	-o $Sample_dir/analysis/FXX.recalibrated.bam \
+ 	-BQSR $Sample_dir/qc/FXX.recal.grp
+
+
+## ????? 
+## Something happens where they go from *recalibrated.bam to *final.bam
+## *final.bam is never created by this code
